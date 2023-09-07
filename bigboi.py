b32036c996e92d772841189aaed5458dc0086719
lucasjorstun2005 <lucas.jorstun@gmail.com>
2023-09-07 13:28:26 +0200

    ye

--- bigboi.py
+++ bigboi.py
@@ -1,30 +1,8 @@
 
 name = input("Vad är ditt namn? ")
-#color = input("Vad är din favoritfärg? ")
-#mat = input("Vad är din favoritmat? ")
 cash = int(input("Hur mycket pengar har du? "))
 
-# Nigga
-'''print(name, "din favoritfärg är " + color) 
-print(name, "är ett fint namn.")
-print(name, mat, "är gott.")'''
 if cash > 100000:
     print(name, "du är rik. Man kommer långt med", str(cash) + "kr på kontot")
 else:
     print(name, "du är inte rik. Man kommer inte långt med", str(cash) + "kr på kontot")
-
-'''
-bas = float(input("Vad är triangelns bas? "))
-höjd = float(input("Vad är triangelns höjd? "))
-
-area = bas * höjd / 2
-print(area)
-'''
-'''
-radie = float(input("Vad är cirkelns radie? "))
-
-area = radie * radie * 3.14
-omkrets = 2 * radie * 3.14
-
-print("Arean är", area, "och omkretsen är", omkrets)
-'''

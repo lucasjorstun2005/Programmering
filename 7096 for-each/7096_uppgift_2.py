5c2769a43bf7811e197c07765f8a6afa93cfd1de
lucasjorstun2005 <lucas.jorstun@gmail.com>
2023-10-19 13:48:39 +0200

    hej

---
+++ 7096 for-each/7096_uppgift_2.py
@@ -0,0 +1,8 @@
+resultat = '13324343532611465515256554532451154255241636666454'
+sexor = 0
+for i in resultat:
+    if i == "6":
+        sexor = sexor + 1
+    else:
+        pass
+print(f"Det är {sexor} sexor.")

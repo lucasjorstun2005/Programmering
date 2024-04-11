60aa42ec6398ef2d4491908b30f20a499793756a
Lucas Jörstun <lucas.jorstun05@skola.stenungsund.se>
2024-04-11 13:48:09 +0200

    Move

--- Tic tac toe/tictactoe.py
+++ Tic tac toe/tictactoe.py
@@ -6,6 +6,22 @@ class Game(object):
         self.players = [player1, player2]
         self.turn = 0
 
+    def greet_user(self, currplayer):
+        print("Det är din tur " + currplayer.symbol)
+
+    def play(self):
+        flag = False
+
+        while flag == False:
+            currplayer = self.players[self.turn]
+
+            self.board.print_board()
+
+            self.greet_user(currplayer)
+
+            move = Move(self.board, currplayer)
+            player_move = move.ask_move()
+
 class Player(object):
     pass
 
@@ -33,7 +49,7 @@ class Board(object):
     # Organiserar rader så att det är tre rutor i varje rad
     def organize_board(self):
         board = [self.tiles[0], self.tiles[1], self.tiles[2]], [self.tiles[3], self.tiles[4], self.tiles[5]], [self.tiles[6], self.tiles[7], self.tiles[8]]
-
+    
         return board
     
     def print_board(self):
@@ -47,7 +63,32 @@ class Board(object):
             sys.stdout.write('\n')
 
 class Move(object):
-    pass
+    def __init__(self, board, player):
+        self.board = board
+        self.player = player
+    
+    def ask_move(self):
+        flag = False
+        possible_moves = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+        while flag != True:
+            move = input("Vänligen ange numret dit du vill flytta din" + self.player.symbol + ":")
+
+            # Kolla efter om använder har angett ett nummer mellan 0-8, om så, konvertera det till int
+            if move in possible_moves:
+                move = int(move)
+            # Annars, be användaren att ange ett nytt nummer
+            else:
+                print("Vänligen ange ett nummer mellan 0-8")
+                return self.ask_move()
+            
+            if move != self.board.tiles[move]:
+                print("Det har redan gjorts ett drag här, försök igen:")
+                return self.ask_move()
+            
+            return move
+
+
 
 player1 = HumanPlayer('X')
 player2 = HumanPlayer('O')
